7bb0033f83616c03d5d5b9ced9519232827a0912
Sahil Duwal <sahilduwal@gmail.com>
2025-06-18 23:13:00 +0545

    Working portfolio website

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path"; // ✅ Required to resolve the alias
 
 export default defineConfig({
-  base: "/portfolio/", // or your repo name if deploying to GitHub Pages
+  base: "/Portfolio/", // or your repo name if deploying to GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
